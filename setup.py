cb8ee0985c4496db4496841cd73f3e2fa0a0dce1
ersul4ik <ersul4ik@mail.ru>
2018-11-28 11:12:00 +0600

    Updated the code in setup.py #15

--- setup.py
+++ setup.py
@@ -1,13 +1,41 @@
 import setuptools
 
-setuptools.setup(
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+:copyright: (c) 2017 by Lev Lazinskiy
+:license: MIT, see LICENSE for more details.
+"""
+import os
+import sys
+
+from setuptools import setup
+from setuptools.command.install import install
+
+# circleci.py version
+VERSION = "1.1.1"
+
+class VerifyVersionCommand(install):
+    """Custom command to verify that the git tag matches our version"""
+    description = 'verify that the git tag matches our version'
+
+    def run(self):
+        tag = os.getenv('CIRCLE_TAG')
+
+        if tag != VERSION:
+            info = "Git tag: {0} does not match the version of this app: {1}".format(
+                tag, VERSION
+            )
+            sys.exit(info)
+
+setup(
     name="swc-registry",
-    version="0.0.3",
+    version=VERSION,
     url="https://github.com/SmartContractSecurity/SWC-registry-python",
     author="SmartContractSecurity",
     author_email="ersul4ik@gmail.com",
     description="Python library for accessing SWC-registry content.",
-    long_description=open("README.rst").read(),
+    long_description=open("README.md").read(),
     packages=["swc_registry"],
     include_package_data=True,
     install_requires=["requests==2.20.1"],
@@ -19,4 +47,7 @@ setuptools.setup(
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
+    cmdclass={
+        'verify': VerifyVersionCommand,
+    }
 )

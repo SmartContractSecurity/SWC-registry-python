d935142b74df9d5c23253e4543929f091cc876d5
ersul4ik <ersul4ik@mail.ru>
2019-01-04 12:07:47 +0600

    Updated the product code for use auto-incrementation package version

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # The Python SWC Registry Library
 
 
-[![CircleCI](https://circleci.com/gh/SmartContractSecurity/SWC-registry-python.svg?style=svg)](https://circleci.com/gh/SmartContractSecurity/SWC-registry-python)
+[![CircleCI](https://circleci.com/gh/ersul4ik/SWC-registry-python.svg?style=svg)](https://circleci.com/gh/SmartContractSecurity/SWC-registry-python)
 
 ## Description
 Python library for accessing SWC-registry content.
@@ -12,12 +12,28 @@ Python library for accessing SWC-registry content.
 $ pip install -U swc-registry
 ```
 
-## Usage
+## Example
 ```python
 from swc_registry import SWC
 
 swc = SWC('SWC-100')
-swc.title
+print(swc.title)
+
+// Function Default Visibility
 ```
 
+
+## Behaviour
+
+On first use of the SWC methods, the SWC registry is initialized from file (swc-definition.json) out cache. If user wants to get the latest information about SWC-registry he needs to pass the second argument of SWC class.
+
+### Get latest version
+```python
+from swc_registry import SWC
+
+swc = SWC('SWC-100', True)
+print(swc.title)
+
+// Function Default Visibility
+```
 ## Contribution
--- setup.py
+++ setup.py
@@ -6,35 +6,15 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-# circleci.py version
-VERSION = "0.0.6"
-
-from os import path
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-class VerifyVersionCommand(install):
-    """Custom command to verify that the git tag matches our version"""
-    description = 'verify that the git tag matches our version'
-
-    def run(self):
-        tag = os.getenv('CIRCLE_TAG')
-
-        if tag != VERSION:
-            info = "Git tag: {0} does not match the version of this app: {1}".format(
-                tag, VERSION
-            )
-            sys.exit(info)
 
 setup(
     name="swc-registry",
-    version=VERSION,
+    version="0.0.6",
     url="https://github.com/SmartContractSecurity/SWC-registry-python",
     author="SmartContractSecurity",
     author_email="ersul4ik@gmail.com",
-    description="Python library for accessing SWC-registry content.",
-    long_description=long_description,
+    description="The python library for accessing SWC-registry content.",
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=["swc_registry"],
     include_package_data=True,
@@ -47,7 +27,5 @@ setup(
     ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
-    cmdclass={
-        'verify': VerifyVersionCommand,
-    }
+    
 )
